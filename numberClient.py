07dd54ae2b1e04703915059970252b17a348e912
Spacenin <chepstudios@gmail.com>
2023-03-11 20:20:54 -0500

    'rebasing'

--- numberClient.py
+++ numberClient.py
@@ -8,7 +8,7 @@ def run():
     secretData = None
 
     #Open secrets file and store it into json
-    with open("/home/pi/Code/numbersApp/numbersApp/secrets.json", "r") as secretsFile:
+    with open("/home/pi/Code/numbersApp/secrets.json", "r") as secretsFile:
         secretData = json.load(secretsFile)
 
     #Get the numbers
